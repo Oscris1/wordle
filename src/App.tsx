50d7f80b46496708a8626b417ea2508c34492688
Damian <damian.pietka@codema.pl>
2022-04-16 12:28:18 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import {theme} from './theme';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.rootContainer}>
+      <StatusBar barStyle={'light-content'} />
+      <Text style={styles.header}>WORDLE</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+  header: {
+    color: theme.colors.text,
+    alignSelf: 'center',
+    fontSize: 30,
+    fontWeight: '900',
+  },
+});
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+interface ThemeInterface {
+  colors: {
+    background: string;
+    text: string;
+  };
+}
+
+export const theme: ThemeInterface = {
+  colors: {
+    background: 'black',
+    text: 'white',
+  },
+};
